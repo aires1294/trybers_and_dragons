5361f7488ed6802f55242747286b93e83e540648
aires1294 <pedrotja.engenharia@gmail.com>
2023-03-28 23:11:02 -0300

    item 6 feitooo

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -4,8 +4,8 @@ export default interface Fighter {
   lifePoints: number;
   strength: number;
   defense: number;
-  energy: Energy;
-  attack(enemy: Fighter): void;
+  energy?: Energy;
+  attack?(enemy: Fighter): void;
   special(enemy: Fighter): void;
   levelUp(): void;
   receiveDamage(attackPoints: number): number;
